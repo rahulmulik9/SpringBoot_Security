87e09ca61094bc501ba2392710e9a13334fb7a3e
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-01 20:19:41 +0530

    Added RegisterationCompleteEventListerner

---
+++ src/main/java/com/rahul/Security/entity/listner/RegisterationCompleteEventListerner.java
@@ -0,0 +1,19 @@
+package com.rahul.Security.entity.listner;
+
+import com.rahul.Security.entity.User;
+import com.rahul.Security.event.RegisterationComleteEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.util.UUID;
+
+public class RegisterationCompleteEventListerner implements ApplicationListener<RegisterationComleteEvent> {
+    @Override
+    public void onApplicationEvent(RegisterationComleteEvent event) {
+        //create verification token with user
+        User user = event.getUser();
+        String toekn = UUID.randomUUID().toString();
+
+        //send mail to user
+
+    }
+}
--- src/main/java/com/rahul/Security/event/RegisterationComleteEvent.java
+++ src/main/java/com/rahul/Security/event/RegisterationComleteEvent.java
@@ -1,8 +1,12 @@
 package com.rahul.Security.event;
 
 import com.rahul.Security.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.ApplicationEvent;
 
+@Getter
+@Setter
 public class RegisterationComleteEvent extends ApplicationEvent {
     private User user;
     private String applicationURL;
