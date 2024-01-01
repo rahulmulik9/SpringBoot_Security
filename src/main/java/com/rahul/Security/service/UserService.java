acffe72b4b860d32bc4e18d009287d41a6049040
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-01 19:53:58 +0530

    Added user service and Repository

---
+++ src/main/java/com/rahul/Security/service/UserService.java
@@ -0,0 +1,4 @@
+package com.rahul.Security.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/rahul/Security/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.rahul.Security.service;
+
+public class UserServiceImpl implements UserService{
+}
