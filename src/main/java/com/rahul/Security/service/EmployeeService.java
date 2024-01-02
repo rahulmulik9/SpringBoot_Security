d2f121c7dc79c138015265f0e631599b21a63410
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-02 21:21:23 +0530

    Java pojo class and simple rest api

---
+++ src/main/java/com/rahul/Security/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.rahul.Security.service;
+import com.rahul.Security.entity.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
