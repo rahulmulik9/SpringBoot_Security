39b4548e73b0d0def5933f9d57735b7dea10736c
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-06 23:52:43 +0530

    Initial commit

--- src/main/java/com/rahul/Security/controller/WelcomeBro.java
+++ src/main/java/com/rahul/Security/controller/WelcomeBro.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeBro {
     @GetMapping("/welcome")
-    public String sayWelcome(){
+    public String sayWelcome() {
         return "Welcome to Spring Application with Security";
     }
 
     //when you start the application the password is in the console.
     //username  is by default user
+
+    //if you want to change the default username and password
+    //then changes in the application properties
+//spring.security.user.name=Rahu
+//spring.security.user.password=123456
 }
