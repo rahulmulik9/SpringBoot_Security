3ccb76b47be648c69223762ce7ce9bfcef2afd40
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-07 16:35:01 +0530

    Implemented user Controller

--- src/main/java/com/rahul/Security/config/SecurityConfig.java
+++ src/main/java/com/rahul/Security/config/SecurityConfig.java
@@ -28,10 +28,9 @@ public class SecurityConfig {
          *
          */
 
-
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf().disable().authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()  //for password and username
-                        .requestMatchers("/notices", "/contact").permitAll())                                //for all
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())                                //for all
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
@@ -87,7 +86,7 @@ public class SecurityConfig {
 //        return new JdbcUserDetailsManager(dataSource);
 //    }
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
