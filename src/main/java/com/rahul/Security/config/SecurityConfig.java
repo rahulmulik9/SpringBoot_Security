33f8196595af69fef61a83afebb4ae0a85256f65
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-07 16:54:14 +0530

    Small changes fixed while checkout and merge

--- src/main/java/com/rahul/Security/config/SecurityConfig.java
+++ src/main/java/com/rahul/Security/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
          *
          */
 
-        http.csrf().disable().authorizeHttpRequests((requests) -> requests
+        http.csrf((csrf)->csrf.disable()).authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()  //for password and username
                         .requestMatchers("/notices", "/contact", "/register").permitAll())                                //for all
                 .formLogin(Customizer.withDefaults())
@@ -47,24 +47,39 @@ public class SecurityConfig {
 
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        //*Approach 1 where we use NoOpPasswordEncoder Bean
-        //while creating the user details*//*
-        //By default this method will encode the password
-        //if you don't want then use {noop} e.g password("{noop}1234")
-        //InMemoryUserDetailsManager can take as many arguments
 
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-    }
+    //if you are using JDBC user management then comment this method
+    //inMemory user management
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        //*Approach 1 where we use NoOpPasswordEncoder Bean
+//        //while creating the user details*//*
+//        //By default this method will encode the password
+//        //if you don't want then use {noop} e.g password("{noop}1234")
+//        //InMemoryUserDetailsManager can take as many arguments
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withUsername("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+    //this is used to get plain text password
+
+
+    //for JDBC user manager
+    //for this database is active and has user table
+    //create table User and files as we do in InMemoryuser manager
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
